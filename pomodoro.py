b8a7c98d4e6a73720652978fe93eea7925c3399f
Attila Szabo <attila.szabo@prezi.com>
2011-11-01 23:22:09 +0100

    5 minute break after pomodoro. Nicer time formatting

--- pomodoro.py
+++ pomodoro.py
@@ -3,20 +3,38 @@ import datetime
 import commands
 
 
-class App():
+class PomodoroTimer():
+    """
+        Simple pomodoro timer app
+    """
     def __init__(self):
         self.root = Tkinter.Tk()
         self.root.title("Pomodoro timer")
         self.label = Tkinter.Label(font=("Helvetica Neue", 44), width=10)
         self.label.pack(ipadx=10, ipady=10, )
-        self.reset_time()
+        self.start_pomodoro()
         self.update_timer()
         self.beeped = False
+        self.break_passed = False
         self.root.mainloop()
 
-    def reset_time(self, event=None):
+    def reset_time(self, event=None, minutes=1):
         self.beeped = False
-        self.end = datetime.datetime.now() + datetime.timedelta(minutes=25)
+        self.end = datetime.datetime.now() + datetime.timedelta(
+                                                        minutes=minutes)
+
+    def start_break(self, event=None):
+        self.reset_time(minutes=5)
+        self.break_passed = True
+        self.beeped = False
+        self.label.configure(fg="dark green")
+        self.label.unbind("<Button-1>")
+
+    def start_pomodoro(self, event=None):
+        self.reset_time(minutes=25)
+        self.label.configure(fg="black")
+        self.break_passed = False
+        self.label.unbind("<Button-1>")
 
     def update_timer(self):
         rem = self.end - datetime.datetime.now()
@@ -24,14 +42,20 @@ class App():
         if(rem > 0):
             hours, remainder = divmod(rem, 3600)
             minutes, seconds = divmod(remainder, 60)
-            text = '%s:%s:%s' % (hours, minutes, seconds)
+            text = '%02d:%02d' % (minutes, seconds)
         else:
-            text = "pomodoro"
             if not self.beeped:
                 commands.getstatusoutput('osascript -e "beep 3"')
                 self.beeped = True
-            self.label.bind("<Button-1>", self.reset_time)
+
+            if not self.break_passed:
+                text = "Break"
+                self.label.bind("<Button-1>", self.start_break)
+            else:
+                text = "Pomodoro"
+                self.label.bind("<Button-1>", self.start_pomodoro)
+
         self.label.configure(text=text)
         self.root.after(1000, self.update_timer)
 
-app = App()
+app = PomodoroTimer()
